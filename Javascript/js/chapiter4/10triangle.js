252271e30a512f22cfaf6cecdbab7f6d7df53c3e
Dany <danyweismail@gmail.com>
2017-01-31 23:12:20 +0100

    chapiter4 finished now i just have to do the test

---
+++ Javascript/js/chapiter4/10triangle.js
@@ -0,0 +1,13 @@
+var i = 1;
+var hash = "#";
+while (i <= 7) {
+    console.log(hash);
+    hash = hash + "#"
+    i++;
+}
+
+var hashtag = "#"
+for (var i = 1; i <= 7; i++) {
+    console.log(hashtag);
+    hashtag = hashtag + "#";
+}
