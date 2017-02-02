76945dbbc1371c94084abd1cd46d8e8160e61202
Dany <danyweismail@gmail.com>
2017-02-02 13:45:44 +0100

    exercise fini

--- Javascript/js/exercise1.js
+++ Javascript/js/exercise1.js
@@ -15,53 +15,22 @@ console.log('(La solution est ' + solution + ')');
 
 // TODO : complétez le programme
 
-var saisie = prompt('Deviner le numéreau au quel je pense:');
-for(var i = 1; i<= 6; i++){
-  if (saisie < solution){
-    console.log(saisie + ' est trop petit');
-    prompt('encore une fois');
-  } else if (saisie > solution){
-    console.log(saisie + ' est trop grand');
-    prompt('encore une fois');
-  } else if (i === 6){
-    console.log('Perdu');
-  } else {
-    console.log('Bravo ls solution est ' + solution + '!\nEt seulement en ' + i + ' fois');
+var saisie = "";
+var i = 0;
+
+while ((saisie !== solution) && (i < 6)) {
+  i++;
+  saisie = Number(prompt('Deviner le numéreau au quel je pense:'));
+  if (saisie > solution) {
+    console.log(saisie + " est trop");
+  } else if (saisie < solution) {
+    console.log(saisie + " est pas assez");
   }
 }
 
-
-/*while ((i <= 6) || (saisie !== solution)) {
-
-  if (saisie < solution) {
-    console.log('La ' + saisie + 'est trop petite');
-
-    prompt('encore une fois');
-    i++;
-  } else if (saisie > solution) {
-    console.log('La ' + saisie + ' est trop grand');
-    i++;
-    prompt('encore une fois');
-  } else if (i === 6) {
-    console.log('Perdu la solution était ' + solution + ' !');
+  if (saisie === solution) {
+   console.log('Bravo la solution est ' + solution );
+   console.log('est tout ça en ' + i + ' essai(s)');
   } else {
-    console.log('Bravo la solution etait ' + solution + ',\n juste ' + i + ' essai(s)');
+    console.log('Perdu ... la solution était ' + solution);
   }
-}
-/*    if (saisie === solution) {
-        console.log("Bravo la solution etait " + solution + ",/n juste " + i + "essai(s)");
-    } else {
-        if (saisie < solution) {
-            console.log(saisie + " est trop petit!");
-            prompt("en reessaie:");
-            i++;
-        } else if (saisie > solution) {
-            console.log(saisie + " est trop grand!");
-            prompt("en reessaie:");
-            i++;
-        } else {
-            console.log("Perdu .... La solution etait " + solution);
-        }
-    }
-}
-*/
