9ecf87d911727136f7ca9f1a4c654cd2f054b358
Dany <danyweismail@gmail.com>
2017-02-03 19:02:01 +0100

    change pc because of the bad weather

--- Javascript/js/chapiter5/7calc.js
+++ Javascript/js/chapiter5/7calc.js
@@ -1,10 +1,31 @@
 // je dois reflechir pourquoi le signe n'est pas vu comme signe
-function calc(nomb1, signe, nomb2){
-  var total = nomb1 + signe + nomb2;
+function calculer(nomb1, signe, nomb2) {
+
+// EVAL()  fait le calcule de l'interieur d'une array ou d'un objet
+
+  var total = eval(nomb1 + signe + nomb2);
   return total;
+
+  // ou faire avec switch ou if
+  var resultat;
+  switch(signe){
+    case "+":
+    resultat = nomb1 + nomb2;
+    break;
+    case "-":
+    resultat = nomb1 - nomb2;
+    break;
+    case "*":
+    resultat = nomb1 * nomb2;
+    break;
+    case "/":
+    resultat = nomb1 / nomb2;
+    break;
+  }
+  return resultat;
 }
 
-console.log(calc(4, "+", 6));
-console.log(calc(4, "-", 6));
-console.log(calc(2, "*", 0));
-console.log(calc(12, "/", 0));
+console.log(calculer(4, "+", 6));
+console.log(calculer(4, "-", 6));
+console.log(calculer(2, "*", 0));
+console.log(calculer(12, "/", 0));
