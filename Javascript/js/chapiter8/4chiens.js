a924e4d592192a37fd8bcec18d704d96b568642a
Dany <danyweismail@gmail.com>
2017-02-08 22:27:08 +0100

    begining of chiens

---
+++ Javascript/js/chapiter8/4chiens.js
@@ -0,0 +1,28 @@
+var Chien = {
+  initChien: function(nom, race, taille){
+    this.nom = nom;
+    this.race = race;
+    this.taille = 0;
+  },
+  aboyer: function(){
+    if(this.taille >= 50){
+      var aboie = "Grrr ! Grrr !" ;
+      return aboie;
+    } else{
+      aboie = "Kali ! Kali !";
+      return aboie;
+
+    }
+  }
+};
+
+
+var crokdur = Object.create(Chien);
+crokdur.init("Crokdur", "mâtin de Naples", 75);
+console.log(crokdur.nom + " est un " + crokdur.race + " mesurant " + crokdur.taille + " cm.");
+console.log("Tiens, un chat ! " + crokdur.nom + " aboie : " + crokdur.aboyer());
+
+var pupuce = Object.create(Chien);
+pupuce.init("Pupuce", "bichon", 22);
+console.log(pupuce.nom + " est un " + pupuce.race + " mesurant " + pupuce.taille + " cm.");
+console.log("Tiens, un chat ! " + pupuce.nom + " aboie : " + pupuce.aboyer());
