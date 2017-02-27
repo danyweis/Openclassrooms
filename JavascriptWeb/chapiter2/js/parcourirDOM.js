9994ec36dd8ce616526662aa1595606284524f44
Dany <danyweismail@gmail.com>
2017-02-27 14:27:45 +0100

    parcourir le DOM

--- JavascriptWeb/chapiter2/js/parcourirDOM.js
+++ JavascriptWeb/chapiter2/js/parcourirDOM.js
@@ -27,3 +27,70 @@ console.log(document.getElementById("nouvelles"));
 // Tous les éléments fils de l'élément d'identifiant "antiques" ayant la clesse "existe"
 console.log(document.getElementById("antiques").getElementsByClassName("existe").length);
 // Affiche 1
+
+// Tous les paragraphes
+console.log(document.querySelectorAll("p").length);
+// Affiche 3
+
+// Tous les paragraphesà l'intérieur de l'élément identifié par "contenu"
+console.log(document.querySelectorAll("#contenu p").length);
+// Affiche 2
+
+// Tous les éléments ayant la classe "existe"
+console.log(document.querySelectorAll(".existe").length);
+// Affiche 8
+
+// Tous les éléments fils de l'élément identifié par "antiques" ayant la classe "existe"
+console.log(document.querySelectorAll("#antiques > .existe").length);
+// Affiche 1
+
+// Le premier paragraphe
+console.log(document.querySelector("p"));
+
+//
+//
+//Obtenir des informations sur les éléments
+
+// Le contenu HTML
+
+// Le contenu HTML de l'élément identifié par "contenu"
+console.log(document.getElementById("contenu").innerHTML);
+
+// Le contenu textuel
+
+// Le contenu textuel de l'élément identifié par "contenu"
+console.log(document.getElementById("contenu").textContent);
+
+// Les attributs
+
+// L'attribut href du premier lien
+console.log(document.querySelector("a").getAttribute("href"));
+
+// L'identifiant de la première liste
+console.log(document.querySelector("ul").id);
+
+// L'attribut href du premier lien
+console.log(document.querySelector("a").href);
+
+// On peut vérifier le présnce d'un attribute
+if (document.querySelector("a").hasAttribute("target")) {
+    console.log("Le premier lien possède l'attribut target");
+} else {
+    console.log("Le premier lien ne possède pas l'attribut target")
+};
+
+// Les classes
+
+// Liste des classes de l'élément identifié par "antiques"
+var classes = document.getElementById("antiques").classList;
+console.log(classes.length);
+// Affiche 1 : l'élément possède une seule classe
+console.log(classes[0]);
+// Affiche "merveilles"
+
+// Il y a aussi la possibilité de tester la présence  d'une classe dans un élément en appelant la méthode "contains" sur la liste des classes
+if (document.getElementById("antiques").classList.contains("merveille")) {
+    console.log("L'élément identifié par antiques possède la classe merveille");
+} else {
+    console.log("L'élément identifié par antiques ne possède pas la classe merveille");
+}
