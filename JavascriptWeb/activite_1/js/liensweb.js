414dd312ee82b6e64ae096cb5ba0db69a3af391d
Dany <danyweismail@gmail.com>
2017-03-04 22:45:37 +0100

    activite1 there is something I not understand about appendChild

--- JavascriptWeb/activite_1/js/liensweb.js
+++ JavascriptWeb/activite_1/js/liensweb.js
@@ -25,3 +25,47 @@ var listeLiens = [
 ];
 
 // TODO : compléter ce fichier pour ajouter les liens à la page web
+
+// création des éléments
+
+var lienElt = document.createElement("span");
+var titreElt = document.createElement("h2");
+var urlElt = document.createElement("p");
+var auteurElt = document.createElement("p");
+var titreCouleur = "#428bca"
+// boucle qui chercher les infos
+
+for (var i = 0; i < listeLiens.length; i++) {
+
+    lienElt.setAttribute("class", "lien");
+    
+    titreElt.setAttribute("class", "titre");
+    titreElt.href = listeLiens[i].url;
+    titreElt.textContent = listeLiens[i].titre;
+    
+   
+
+    //urlElt.textContent = titreElt.textContent + " " + listeLiens[i].url;
+    //urlElt.textContent =  listeLiens[i].url;
+
+    auteurElt.textContent = "Ajouté par " + listeLiens[i].auteur
+    var topElt = document.createElement("span");
+    topElt.textContent = titreElt + listeLiens[i].url;
+    lienElt.appendChild(topElt);
+    //lienElt.appendChild(titreElt);
+    //lienElt.appendChild(urlElt);
+    lienElt.appendChild(auteurElt);
+
+    sendInfo(lienElt);
+
+};
+
+
+function sendInfo(info) {
+document.getElementById("contenu").appendChild(lienElt);
+}
+ 
+// CSS
+
+lienElt.style.display = "block"; 
+titreElt.style.color = titreCouleur;
