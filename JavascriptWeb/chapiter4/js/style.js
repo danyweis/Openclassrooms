d3b9523243e6d389efd1b0722aeddcfca70d1279
danyweis <danyweismail@gmail.com>
2017-03-04 10:06:41 +0100

    debut chapiter4

---
+++ JavascriptWeb/chapiter4/js/style.js
@@ -0,0 +1,8 @@
+var pElt = document.querySelector('p');
+pElt.style.color = "red";
+pElt.style.margin = "50px";
+
+// les noms composées comme font-family ou background-color
+
+pElt.style.fontFamily = "Arial";
+pElt.style.backgroundColor = "black";
