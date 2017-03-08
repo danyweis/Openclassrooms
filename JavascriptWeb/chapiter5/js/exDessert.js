e28dffd27e917016f21cbeeb3f98eab2b4507580
danyweis <danyweismail@gmail.com>
2017-03-08 10:30:12 +0100

    exercice dessert i can not do the addEventListener to the li

--- JavascriptWeb/chapiter5/js/exDessert.js
+++ JavascriptWeb/chapiter5/js/exDessert.js
@@ -1,18 +1,60 @@
 var boutonElt = document.querySelector("button");
 var list = document.getElementById("desserts");
+var dessert = "";
+
+
+// input
+function inputElt(){
+	
+	var textInput = document.createElement("input");
+	var okBtn = document.createElement("button");
+	okBtn.id = "btn";
+	okBtn.textContent = "Send";
+	textInput.setAttribut = ("type", "text");
+	textInput.id = "la";
+	list.appendChild(textInput);
+	list.appendChild(okBtn);
+	okBtn.addEventListener("click", function(){
+		console.log("ok");
+		dessert = document.createElement("li");
+		dessert.textContent = textInput.value;
+		dessert.classList = "leDessert";
+		list.appendChild(dessert);
+		console.log(dessert);
+		list.removeChild(textInput);
+		list.removeChild(okBtn);
+	});
+	
+};
+//inputElt();
+
 
 // Ajouter un dessert
 boutonElt.addEventListener("click", function() {
-	//var ajoutDessert = prompt("Ajouter un dessert:");
-	var dessert = document.createElement("li");
-	dessert.textContent = prompt("Ajouter un dessert:");
-	list.appendChild(dessert);
-	console.log(dessert);
+	inputElt();
 });
 
-// Modifier un dessert
-function changeDessert(){
-	document.addEventListener("click" function(e){
 
-	});
+// Modifier un dessert
+/*
+document.getElementsByName(dessert).addEventListener("click", function() {
+	
+	console.log("ok");
+});*/
+/*test.addEventListener("click", function() {
+	
+	console.log("ok");
+});
+addEventListener("click", function () {
+	console.log("Gestionnaire paragraphe");
+});
+
+*/
+var test = document.getElementsByClassName("leDessert");
+
+for (var i = 0; i<test.length; i++){
+	
+	test[i].addEventListener("click", function() {
+		console.log(test.length);
+	})
 }
