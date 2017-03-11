2c85a7b8682b4c131596299a7e8cb09a9ba0787b
danyweis <danyweismail@gmail.com>
2017-03-11 14:54:21 +0100

    debut des formulaires

--- JavascriptWeb/chapiter6/js/formulaire.js
+++ JavascriptWeb/chapiter6/js/formulaire.js
@@ -14,3 +14,50 @@ var pseudoElt = document.getElementById("pseudo");
 pseudoElt.value = "MonPseudo";
 
 // ==> Gestion du focus
+
+// Affiche d'un message contextuel pour la saisie du pseudo
+
+pseudoElt.addEventListener("focus", function(){
+	document.getElementById("aidePseudo").textContent = "Entrez votre pseudo";
+});
+
+// Suppression du message contextuel pour la saisie du pseudo
+
+pseudoElt.addEventListener("blur", function (e) {
+	document.getElementById("aidePseudo").textContent = "";
+});
+
+//il se met automatiquement sur l'element des chargement de la page
+pseudoElt.focus();
+
+
+// ==> LES ELEMENT D'OPTIONS <== //
+
+// ==> Cases à cocher
+
+//	Lorsque la case change de valeur, l'objet "Event" 
+//	associé à l'événement dispose d'une propriété 
+//	booléenne "checked" qui indique le nouvel état de 
+//	la case (cochée/décochée).
+
+// Affichage de la demande de confirmation d'inscription
+document.getElementById("confirmation").addEventListener("change", function(e) {
+	console.log("Demande de confirmation : " + e.target.checked);
+});
+
+// ==> Bouton radio
+
+// Affichage du type d'abonnement choisi
+var aboElts = document.getElementsByName("abonnement");
+for (var i = 0; i < aboElts.length; i++) {
+	aboElts[i].addEventListener("change", function(e) {
+		console.log("Formule d'abonnement choisi : " + e.target.value);
+	});
+}
+
+// ==> Listes déroulantes
+
+// Affichage du code de nationalité choisi
+document.getElementById("nationalite").addEventListener("change", function (e) {
+	console.log("Code de nationalité : " + e.target.value);
+});
