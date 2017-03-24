2eed69d84cc1f6163aef851ce240bba4a0eec4f7
Dany <danyweismail@gmail.com>
2017-03-24 22:05:05 +0100

    XMLHttpRequest errors

--- JavascriptWeb/chapiter8/js/webServ.js
+++ JavascriptWeb/chapiter8/js/webServ.js
@@ -26,6 +26,7 @@ req.send(null);
 
 // ==< Gestion des ERREURS
 
+/*
 var req = new XMLHttpRequest();
 req.open("GET", "http://localhost:8888/javascript-web-srv/data/langages.txt");
 req.addEventListener("load", function() {
@@ -42,3 +43,26 @@ req.addEventListener("error", function() {
     console.error("Erreur réseau");
 });
 req.send(null);
+*/
+
+// ==> Écrire une fonction AJAX générique
+
+// Exécute un appel AJAX GET
+// Prnd en paramètre l'URL cible et la fonction callback appelée en cas de succès
+
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function() {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant le réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function() {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
