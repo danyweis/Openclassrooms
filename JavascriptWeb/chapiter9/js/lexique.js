3b547d84ddc0ce225a6ae640c4eccfc0a73f8bcf
Dany <danyweismail@gmail.com>
2017-04-04 21:36:23 +0200

    animer ballon

--- JavascriptWeb/chapiter9/js/lexique.js
+++ JavascriptWeb/chapiter9/js/lexique.js
@@ -37,5 +37,41 @@ console.log(debut);
 
 document.getElementById("choix").addEventListener("click", function(e) {
     var targetElt = event.target;
-    console.log(targetElt.textContent);
+    if (targetElt.textContent !== "A" && targetElt.textContent !== "B" && targetElt.textContent !== "C" && targetElt.textContent !== "D" && targetElt.textContent !== "E") {
+        targetElt = "";
+    } else {
+        appelle(targetElt.textContent);
+
+    }
+
 });
+
+function appelle(quoi) {
+    console.log(quoi);
+    ajaxGet("https://oc-jswebsrv.herokuapp.com/api/lexique/" + quoi, function(reponse) {
+        var mot = JSON.parse(reponse)
+        console.log(mot);
+
+
+
+        //var box = document.createElement("div");
+
+        for (var i = 0; i < mot.length; i++) {
+            //if (mot[i].term === "") {
+            //console.log(mot[i].term);
+            //} else {
+            var term = document.createElement("dt");
+            term.textContent = mot[i].term;
+            var def = document.createElement("dd");
+            def.textContent = mot[i].definition;
+
+            console.log(term);
+            console.log(def);
+            // box.appendChild(term);
+            // box.appendChild(def);
+            //}
+        }
+
+
+    });
+}
