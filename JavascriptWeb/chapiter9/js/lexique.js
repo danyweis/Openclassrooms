61cca48fb7a7355f77b3df84f7a85b78a6e01e29
Dany <danyweismail@gmail.com>
2017-04-06 21:59:58 +0200

    activite API

--- JavascriptWeb/chapiter9/js/lexique.js
+++ JavascriptWeb/chapiter9/js/lexique.js
@@ -1,39 +1,4 @@
-/*
-var debut = document.createElement("p");
-var lexA = document.createElement("a");
-lexA.textContent = "A";
-lexA.id = "lexA";
-var lexB = document.createElement("a");
-lexB.textContent = "B"
-lexB.id = "lexB";
-var lexC = document.createElement("a");
-lexC.textContent = "C";
-lexC.id = "lexC"
-var lexD = document.createElement("a");
-lexD.textContent = "D";
-lexD.id = "lexD";
-var lexE = document.createElement("a");
-lexE.textContent = "E";
-lexE.id = "lexE";
-var points = document.createElement("span");
-points.textContent = "...";
-var sep = document.createElement("span");
-sep.textContent = " | ";
-debut.appendChild(lexA);
-debut.appendChild(sep);
-debut.appendChild(lexB);
-debut.appendChild(sep);
-debut.appendChild(lexC);
-debut.appendChild(sep);
-debut.appendChild(lexD);
-debut.appendChild(sep);
-debut.appendChild(lexE);
-debut.appendChild(sep);
-debut.appendChild(points);
-var contenu = document.getElementById("contenu");
-contenu.appendChild(debut);
-console.log(debut);
-*/
+// ==> MOI <==
 
 document.getElementById("choix").addEventListener("click", function(e) {
     var targetElt = event.target;
@@ -75,3 +40,47 @@ function appelle(quoi) {
 
     });
 }
+
+// ==> OPC <==
+
+var motsElt = document.getElementById("mots");
+
+function afficherMots(lettre) {
+  ajaxGet("http://oc-jswebsrv.herokuapp.com/api/lexique/" + lettre, funktion(reponse){
+          var mots = JSON.parse(reponse);
+          motsElt.innerHTML = "";
+          if (mots.length > 0){
+          mots.forEach(function (mot) {
+          var termeElt = document.createElement("h3");
+            termeElt.textContent = mot.term;
+            var definitionElt = document.createElement("div");
+            definitionElt.textContent = mot.definition;
+            motsElt.appendChild(termeElt);
+            motsElt.appendChild(definitionElt);
+          });
+          } else {
+            var msgElt = document.createElement("p");
+            msgElt.textContent = "Aucun mot trouvé pour " + lettre;
+            motsElt.appendChild("msgElt");
+          }
+          });
+}
+
+var lettres = ["A", "B", "C", "D", "E"];
+lettres.forEach(function (lettre) {
+  var lettreElt = document.createElement("a");
+  lettreElt.textContent = lettre;
+  lettreElt.href = "#";
+  lettreElt.addEventListener("click", function() {
+    afficherMots(lettre);
+  });
+  lettresElt.appendChild(lettreElt);
+  lettresElt.appendChild(document.createTextNode(" | "));
+});
+lettresElt.appendChild(document.createTextNode("..."));
+
+
+
+
+
+
