7592e2012b46ee017b24fd64f44e21f14d1f1419
danyweis <danyweismail@gmail.com>
2017-10-16 22:03:58 +0100

    Start learning Python after long break (and it feels good to code again)

---
+++ Python/chapitre1/1conditionnelleTP.py
@@ -0,0 +1,51 @@
+# -*-coding:Utf-8 -*
+
+
+#Exercice que j'ai fais moi même
+annee = input("Entre une année pour voir si elle est bissextile...:")
+
+lannee = annee
+annee = int(annee)
+
+if annee % 4 != 0:
+  print("L'année ", lannee, " n'est pas bissextile")
+else:
+  if annee % 100 == 0:
+    if annee % 400 == 0:
+      print("L'année ", lannee, " est bissextile")
+    else:
+      print("L'année ", lannee, " n'est pas bissextile")
+  else:
+    print("L'année ", lannee, " est bissextile")
+
+#Correction 1
+
+annee = input("Saisissez une année : ")
+
+annee = int(annee)
+bissextile = False
+
+if annee % 400 == 0:
+  bissextile = True
+elif annee % 100 == 0:
+  bissextile = False
+elif annee % 4 == 0:
+  bissextile = True
+else:
+  bissextile = False
+
+if bissextile: #Si l'annee est bissextile
+  print("L'année saisie est bissextile")
+else:
+  print("L'année saisie n'est pas bissextile")
+
+#Correction 2
+
+annee = input("Saisissez une année : ")
+
+annee = int(annee)
+
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
+  print("L'année saisie est bissextile")
+else:
+  print("L'année saisie n'est pas bissextile")
---
+++ Python/chapitre1/2boucle.py
@@ -0,0 +1,49 @@
+# -*-coding:Utf-8 -*
+'''
+print("La boule \"while\"")
+# boucle while
+
+nb = 7 #On garde la variable contenant le nmbre dont on veut la table de multiplication
+i = 0 # C'est notre variable compteur que nous allons incrémenter dans la boucle
+
+while i < 10: # Tant que i est strictement inférieur à 10
+  print(i + 1, "*", nb, "=", (i + 1) * nb)
+  i += 1 # On incrémente i de 1 à chaque tour de boucle
+
+
+print("La boule \"for\"")
+# boucle for
+
+# for ELEMENT in SEQUENCE
+chaine = "Bonjour les ZEROS"
+for lettre in chaine: # lettre peut aussi etre i
+  print(lettre)
+
+autreChaine = "Salut les LOULOUS"
+for autreLettre in autreChaine:
+  if autreLettre in "AEIOUYaeiouy": # autreLettre est une voyelle
+    print(autreLettre)
+  else:
+    print("*")
+'''
+print("Le mot clé \"break\"")
+# mot clé break
+
+#LA BOUCLE TEMPS QU'ON TAPE PAS Q JUSTE POUR EXEMPLE
+while 1: # 1 est toujours vrai => boucle infinie
+  lettre = input("Tapez 'Q' pour quitter : ")
+  if lettre == "Q":
+    print("Fin de la boucle")
+    break
+
+print("Le mot clé \"continue\"")
+# mot clé continue
+
+i = 1
+while i < 20: # Tant que i est inferieur à 20
+  if i % 3 == 0:
+    i += 4 # On ajoute 4 à i
+    print("On incrémente i de 4. i est maintenant égale à", i)
+    continue # On retourne au while sans exécuter les autres lignes
+  print("La variable i =", i)
+  i += 1 # Dans le cas classique on ajoute juste 1 à i
