e201d32fc37382407a8a8de0c451d38d11279b78
danyweis <danyweismail@gmail.com>
2017-10-18 23:17:42 +0100

    les fonctions demain les fonction lambda

--- Python/chapitre1/3functions.py
+++ Python/chapitre1/3functions.py
@@ -1,5 +1,5 @@
 # -*- coding:Utf-8 -*
-'''
+
 def tablePar7():
   nombre = 7
   i = 0 # Notre compteur ! 
@@ -18,7 +18,7 @@ def table(nb):
 
 # on appelle la fonction avec un argument
 table(5)
-'''
+
 
 def tableMax(multip, max):
   i = 0
@@ -28,3 +28,38 @@ def tableMax(multip, max):
 
 # On appelle la fonction avec le multiplicateur et le maximum en argument
 tableMax(9, 22) 
+
+def tableParDefaut(nombreChoisi, defaut = 10): # ci l'utilisateur donne pas de max le max va etre egal a defaut
+  i = 0
+  while i < defaut:
+    print(i + 1, "*", nombreChoisi, "=", (i + 1) * nombreChoisi)
+    i += 1
+
+tableParDefaut(3, 5) # dans ce cas 10 est remplacer par 5 
+tableParDefaut(3) # dans ce cas trois est multiplier par 10
+
+# comment appeller les fonctions
+
+def fonc(a = 1, b = 2, c = 3, d = 4, e = 5):
+  print("a =", a, "b =", b, "c =", c, "d =", d, "e =", e)
+
+
+'''
+  on peut l'appeller de plusieur facon, voici quelque une:
+
+fonc()  # RESULTAT a = 1 b = 2 c = 3 d = 4 e = 5
+fonc(4) # RESULTAT a = 4 b = 2 c = 3 d = 4 e = 5
+fonc(b = 8, d = 5)  # RESULTAT a = 1 b = 8 c = 3 d = 5 e = 5
+fonc(b = 35, c = 48, a = 4, e = 9)  # RESULTAT a = 4 b = 35 c = 48 d = 4 e = 9
+
+'''
+
+# RETURN
+def carre(valeur):
+  return valeur * valeur
+
+variable = carre(5) # variable est dans ce cas 25
+
+print(variable)
+
+# Les fonctions LAMBDA
