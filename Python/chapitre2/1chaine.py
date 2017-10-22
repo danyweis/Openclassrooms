81164f43c00268ecb3b5ef17e9b5bb43aa3ce00f
danyweis <danyweismail@gmail.com>
2017-10-22 23:14:06 +0100

    chaine de caracteres

---
+++ Python/chapitre2/1chaine.py
@@ -0,0 +1,21 @@
+# -*-coding:utf-8 -*
+
+# EN PYTHON TOUT EST OBJET AUSSI LES CHAINES DE CARACTERES
+
+# DANS L'INTERPRETEUR type(chaine) DONNE COMME RESULTAT <class 'str'>
+
+chaine = "NE CRIE PAS SI FORT !"
+chaine.lower() # METTRE LA CHAINE EN MINUSCULE
+# RESULTAT : ne crie pas si fort !
+
+"""
+    => class = model pour construir un objet => 'str' ici un string
+
+    => chaine = objet
+    => lower  = methode
+    POUR APPELER UNE METHODE SUR UN OBJET
+    => objet.methode()
+"""
+
+
+# METTRE EN FORME UNE CHAINE 
