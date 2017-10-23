af02b686005a7624625ece78b893132417e0cb29
danyweis <danyweismail@gmail.com>
2017-10-23 23:06:39 +0100

    caines de caractaires

--- Python/chapitre2/1chaine.py
+++ Python/chapitre2/1chaine.py
@@ -19,3 +19,73 @@ chaine.lower() # METTRE LA CHAINE EN MINUSCULE
 
 
 # METTRE EN FORME UNE CHAINE 
+
+chaine = str() # CREE UNE CHAINE VIDE AUTRE POSSIBILITE chaine = ""
+while chaine.lower() != "q": # TANT QUE CHAINE N'EST PAS 'Q' OU 'q'
+    print("Tapez 'Q' pour quitter...")
+    chaine = input()
+print("Merci !")
+
+
+# D'AUTRE FACON 
+minuscules = "une chaine en minuscules"
+print(minuscules.upper()) # UNE CHAINE EN MAJUSCULES
+print(minuscules.capitalize()) #LA PREMIERE LETTRE EN MAJUSCULE
+
+espaces = "     une chaine avec des espaces     "
+print(espaces.strip()) # ON RETIRE LES ESPACES AU DEBUT ET A LA FIN
+
+titre = "introduction"
+print(titre.upper().center(20))
+# center(20) VEUT DIRE ON VEUT QUE INTRODUCTION SOIT AU CENTRE DE 20 
+# CARACTERE ICI INTRODUCTION A 12 DONC ON AJOUTE 4 AU DEBUT ET LA FIN
+
+# POUR VOIR D'AUTRE POSSIBILITE ON UTILISE  help(str) DANS L'INTERPRETEUR
+
+#  => FORMATER ET AFFICHER UNE CHAINE <=
+
+prenom = "Paul"
+nom = "Dupont"
+age = 21
+print("Je m'appelle {0} {1} et j'ai {2} ans.".format(prenom, nom, age))
+# Je mappelle Paul Dupont et j'ai 21 ans.
+
+print("Je m'appelle {0} {1} ({3} {0} pour l'administration) et "\
+"j'ai {2} ans.".format(prenom, nom, age, nom.upper()))
+# Je m'appelle Paul Dupont(DUPONT Paul pour l'administration) et j'ai 21 ans.
+
+date = "Lundi 23 octobre 2017"
+heure = "10:20 p.m."
+print("Cela s'est produit le {}, à {}".format(date, heure))
+# Cela s'est produit le Lundi 23 octobre 2017, à 10:20 p.m.
+
+
+# SECONDE SYNTAXE DE LA METHODE format
+
+adresse = """
+{noRue}, {nomRue} 
+{codePostal} {nomVille} ({pays})
+""".format(noRue = 5, nomRue = "rue des Postes", codePostal = 75003, nomVille = "Paris", pays = "France")
+print(adresse)
+
+"""
+RESULTAT
+5, rue des Postes
+75003 Paris (France)
+"""
+
+# CONCATENATION DE CHAINE
+
+prenom = "Jean"
+message = "Bonjour"
+chaineComplete = message + prenom # ON UTILISE LE '+' POUR CONCATENER DEUX CHAINES
+print(chaineComplete) # RESULTA : BonjourJean 
+# POUR AJOUTER UN ESPACE ON FAIS COMME EN JAVASCRIPT
+chaineComplete = message + " " + prenom
+print(chaineComplete) # RESULTAT : Bonjour Jean
+
+# CE QUI N'EST PAS POSSIBLE 
+age = 30
+# message = "J'ai " + age + " ans" ==> TypeError : Can't convert 'int' object to str implicitly
+message = "J'ai " + str(age) + " ans"
+print(message) # RESULTAT : J'ai 30 ans
