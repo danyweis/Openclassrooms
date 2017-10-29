af2017d77b0eb3e1de229bfc0b433edf50a1ea99
danyweis <danyweismail@gmail.com>
2017-10-29 22:22:07 +0000

    parametres dans des fonctions

--- Python/chapitre2/4chaineEtListes.py
+++ Python/chapitre2/4chaineEtListes.py
@@ -51,3 +51,43 @@ fonctionIncunnue(3) # RETOURNE J'ai reçu : (3,).
 fonctionIncunnue("a", "b", "c") # RETOURNE J'ai reçu : ("a", "b", "c").
 var = 3.5
 fonctionIncunnue(var, [4], "...") # RETOURNE J'ai reçu : (3.5, [4], "...").
+
+'''
+AUSSI POSSIBLE:
+==> fonctionInconnue(nom, prenom, * commentaires) 
+!!! DANS CE CAS C'EST IMPERATIVE DE PRECISER UN NOM ET UN PRENOM,
+    ET ENSUITE METTRE CE QU'ON VEUX EN COMMENTAIRES
+
+PAS POSSIBLE:
+==> fonctionInconnue(* commentaires, nom, prenom)
+'''
+
+def afficher (* parametres, sep = ' ', fin = '\n'):
+    """ FONCTION CHARGEE DE REPRODUIRE LE COMPORTEMENT DE PRINT.
+    ELLE DOIT FINIR PAR FAIRE APPEL A PRINT POUR AFFICHER LE RESULTAT.
+    MAIS LES PARAMETRES DEVRONT DEJA AVOIR ETE FORMATES.
+    ON DOIT PASSER A PRINT UNE UNIQUE CHAINE, EN LUI SPECIFIANT DE NE 
+    RIEN METTRE A LA FIN:
+        print(chaine, end = '')"""
+    
+    # LES PARAMETRES SONT SOUS LA FORME D'UN TUPLE
+    # OR ON A BESOIN DE LES CONVERTIR
+    # MAIS ON NE PEUT PAS MODIFIER UN TUPLE
+    # ON A PLUSIEURS POSSIBILITES, ICI JE CHOISIS DE CONVERTIS LE TUPLE EN LISTE
+    parametres = list(parametres)
+    # ON VA COMMENCER PAR CONVERTIR TOUTES LES VALEURS EN CHAINE
+    # SINON ON VA AVOIR QUELQUES PROBLEMES LORS DU "join"
+    for i, parametre in enumerate(parametres):
+        parametres[i] = str(parametre)
+    # LA LISTE DES PARAMETRES NE CONTIENT PLUS QUE DES CHAINES DE CARACTERES
+    # A PRESENT ON VA CONSTITUER LA CHAINE FINALE
+    chiane = sep.join(parametres)
+    # ON AJOUTE LE PARAMETRE FIN A LA FIN DE LA CHAINE
+    chaine += fin
+    # ON AFFICHE L'ENSEMBLE
+    print(chaine, end = '')
+
+# TRANSFORMER UNE LISTE EN PARAMETRES DE FONCTION
+
+listeDesParametres = [1, 4, 9, 16, 25, 36]
+print(* listeDesParametres)
