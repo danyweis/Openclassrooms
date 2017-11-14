1328c5103a93a47eb0676a6d550ad6d74b02d58d
danyweis <danyweismail@gmail.com>
2017-11-14 23:22:45 +0000

    started the hangman game

---
+++ Python/chapitre2/hangman/donnees.py
@@ -0,0 +1,28 @@
+# -*-coding:utf-8 -*
+
+maxChances = 8
+
+joueurConnu = input("To sign in press S\nTo create new account press N\n\
+To see the scores board press B\n To Exit type E\nWhat's your choise :  ")
+
+#commencer = input("Pour commence")
+
+listeMots = ['tanneuse', 'sabotais', 'survolta', 'risquons',
+'couchage', 'cubiques', 'chantent', 'chargiez', 'guichets',
+'relaxent', 'voyantes', 'renflais', 'écrouait', 'suintant',
+'talquons', 'grillant', 'indivise', 'resservi', 'misaines',
+'rupinait', 'bitturas', 'animeras', 'tachines', 'pieuvres',
+'pacquent', 'soufriez', 'pivotiez', 'excipera', 'contexte',
+'lignifie', 'kreutzer', 'encornet', 'houerait', 'voltasse',
+'dystomie', 'transmua', 'remorque', 'ranimant', 'rainetez',
+'insolons', 'apposant', 'doigtais', 'mandchou', 'crispera',
+'différai', 'rompisse', 'projetai', 'cossasse', 'sanglant',
+'explorez', 'odelette', 'empalmes', 'retenait', 'resaluer',
+'mourante', 'peluches', 'mourront', 'puncheur', 'enfoncez',
+'assignez', 'aguerrie', 'confiras', 'quatuors', 'ratatina',
+'servants', 'presseur', 'encoller', 'distinct', 'pointure',
+'chaisier', 'crachons', 'enracine', 'actinies', 'bossette',
+'bombions', 'aseptisa', 'versants', 'valsions', 'farfelus',
+'nappions', 'roderont', 'fuguerai', 'lutinait', 'terserai',
+'expiasse', 'frayasse', 'purgions', 'fauchiez', 'muletier',
+'oeillade', 'empotait', 'pinceras', 'squirrhe', 'usurpiez']
---
+++ Python/chapitre2/hangman/pendu.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8 -*
+
+
+
+print("Bienvenu au jeu PENDU \nLes regles du jeu sont:\nIl faut trouver un mot a 8 lettres\n\
+Tu a 8 chances pour trouver le mot \nchaque tour tu me donne une lettre \nmoi je te montre \
+si la lettre est dans le mot \nsi oui je te l'affiche avec l'emplacement \n\nExemple : g**ll**t\n\n\
+ici tu as déjà trouver le 'g', le 'l' et le 't'\nje pense la meilleur façon c'est de commencer...\n")
+
+'''with open("donnees.py", "r") as test:
+    joueur = test.read()
+'''
+import donnees.py.joueurConnu
