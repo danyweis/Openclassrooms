982962e847448969732e4efcd3d18a14278d690c
danyweis <danyweismail@gmail.com>
2017-11-17 00:27:57 +0000

    try to find out the best way to put in the data in the file

--- Python/chapitre2/hangman/fonctions.py
+++ Python/chapitre2/hangman/fonctions.py
@@ -1,1 +1,5 @@
 # -*-coding:utf-8 -*
+import donnees
+import random
+
+word = random.choice(donnees.listeMots)
--- Python/chapitre2/hangman/pendu.py
+++ Python/chapitre2/hangman/pendu.py
@@ -1,7 +1,7 @@
 # -*-coding:utf-8 -*
 import donnees
 import fonctions
-import random
+
 
 # WELCOME TEXT
 print(donnees.bienvenu)
@@ -11,5 +11,13 @@ if test == "S" or test == "s":
     playerName = input("Pseudo : ")
 elif test == "N" or test == "n":
     playerName = input("Tu veux jouer sous quel pseudo : ")
+elif test == "B" or test == "b":
+    with open("scores", "r") as sBoard:
+        scoreBoard = sBoard.read()
+    print(scoreBoard)
+#elif test == "E" or test == "e":
+    # EXIT THE PROGRAM
 
-print(playerName)
+
+
+print(fonctions.word)
