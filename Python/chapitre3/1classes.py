ee899d78fabf79b94af18e42975ba5285557dfdc
danyweis <danyweismail@gmail.com>
2017-12-26 21:49:25 +0000

    about classes => have to researche a bit more cause dont understand cls

--- Python/chapitre3/1classes.py
+++ Python/chapitre3/1classes.py
@@ -59,3 +59,135 @@ ben = AutrePersonne("Micado", "Ben")
 print(ben.nom) # RETOURNE 'Micado'
 print(ben.prenom) # RETOURNE 'Ben'
 print(ben.age) # RETOURNE 33
+
+# ATTRIBUTS DE CLASSE
+
+class Compteur:
+    """Cette classe possede un attribut de classe qui s'incremente a
+    chaque fois que l'on cree un objet de ce type"""
+
+    objets_crees = 0 # LE COMPTEUR VAUT 0 AU DEPART 
+    def __init__(self):
+        """A chaque fois qu'on cree un objet, on incremente le compteur"""
+        Compteur.objets_crees += 1
+
+print(Compteur.objets_crees) # RETOURNE 0
+a = Compteur()
+print(Compteur.objets_crees) # RETOURNE 1
+b = Compteur()
+print(Compteur.objets_crees) # RETOURNE 2
+
+# A CHAQUE FOIS QUE JE CREE UN OBJET DE TYPE Compteur, LATTRIBUT DE LA 
+# CLASSE objets_crees S'INCREMENTE DE 1
+
+class TableauNoir:
+    """Classe definissant une surface sur laquelle on peut ecrire, que l'on
+    peut lire et effacer, par jeu de methodes. l'attribut modifie est "surface" """
+
+    def __init__(self):
+        """Par default, la surface est vide"""
+        self.surface = ""
+
+    def ecrire(self, message_a_ecrire):
+        """Methode permettant d'ecrire sur la surface du tableau. Si la surface
+        n'est pas vide, on saute une ligne avant de rajouter le message a ecrire"""
+
+        if self.surface != "":
+            self.surface += "\n"
+        self.surface += message_a_ecrire
+
+    def lire(self):
+        """Cette methode se charge d'afficher la surface du tableau"""
+        print(self.surface)
+
+    def effacer(self):
+        """Cette methode se charge d'effacer la surface du tableau"""
+        self.surface = ""
+
+# TEST DE LA CLASSE
+tab = TableauNoir()
+tab.lire()
+tab.ecrire("Cool c'est les vacances!")
+tab.ecrire("Joyeux Noel!")
+tab.lire()
+tab.effacer()
+tab.lire()
+
+help(TableauNoir) # CELA AFFICHE LA DOCUMENTATION SUR NOTRE CLASSES
+
+# METHODES DE CLASSE ET METHODES STATIQUES
+class UnCompteur:
+    """Cette classe possede un attribut de classe qui s'incremente a chaque
+    fois que l'on cree un objet de ce type"""
+
+    objects_crees = 0 
+    def __init__(self):
+        """A chaque fois qu'un objet est creer, le compteur est incrementer"""
+        UnCompteur.objects_crees += 1
+    def combien(cls):
+        """Methode de classe affichant combien d'objets on ete crees"""
+        print("Jusqu'a present, {} objets ont ete crees".format(cls.objects_crees))
+    combien = classmethod(combien)
+    # POUR QUE PYTHON RECONNAISSE UNE METHODE DE CLASSE IL FAUT APPELER LA 
+    # FONCTION "classmethod" QUI PREND EN PARAMETRE LA METHODE QUE L'ON VEUT
+    # CONVERTIR ET RENVOIE LA METHODE CONVERTIE
+
+# LE RESULTAT
+
+UnCompteur.combien()
+a = UnCompteur()
+UnCompteur.combien()
+b = UnCompteur()
+UnCompteur.combien()
+
+# LES METHODES STATIQUE
+
+class Test:
+    """une calsse de test pour les methodes statique qui ne prennent pas de 
+    premier parametre ni 'self' ni 'cls' """
+    def afficher():
+        """Fonction chargee d'afficher quelque chose"""
+        print("On affiche la meme chose")
+        print("peu importe les donnees de l'objet ou de la classe")
+    afficher = staticmethod(afficher)
+
+
+# ==> ICI C'EST EN BONUS PAS IMPORTANT A 100% PEUT ETRE PLUS TARD <==   
+
+# ABOUT THE dir FUNCTION
+class DirTest:
+    """Une classe de test tout simplement"""
+    def __init__(self):
+        """On definit dans le constructeur un unique attribut"""
+        self.mon_attribut = "ok"
+    def afficher_attribut(self):
+        """Methode affichant l'attribut 'mon_attribut' """
+        print("Mon attribut est {0}".format(self.mon_attribut))
+
+# CREONS UN OBJET DE LA CLASSE DirTest
+un_test = DirTest()
+un_test.afficher_attribut() # RENVOIE => Mon attribut est ok
+dir(un_test)
+# LA FONCTION dir RENVOIE UNE LISTE COMPRENANT LE NOM DES ATTRIBUTE
+# ET DES METHODES DE L'OBJET
+# LA FONCTION dir SE CONTENTE DE RENVOYER TOUT CE QU'IL Y A DANS L'OBJET
+
+# L'ATTRIBUT SPECIAL __dict__
+un_test = DirTest()
+un_test.__dict__ # RENVOIE {'mon_attribut': 'ok'}
+
+# ON PEUT AUSSI CHANGER L'ATTRIBUT MAIS C'EST DECONSEILLER
+un_test.__dict__["mon_attribut"] = "plus ok"
+un_test.afficher_attribut() # RENVOIE => Mon attribut est plus ok
+
+# RESUME
+
+# => ON DEFINIT UNE CLASSE AVEC LA SYNTAXE => class NomClasse:
+# => LES METHODES SE DEFINISSENT COMME LES FONCTIONS SAUF QU'ELLES SE TROUVENT
+#   DANS LE CORP DE LA CLASSE
+# => LES METHODES 'INSTANCE PRENNENT EN PREMIER PARAMETRE => self , L'INSTANCE
+#   DE L'OBJET MANIPULE
+# => ON CONSTRUIT UNE INSTANCE DE CLASSE EN APPELANT SON CONSTRUCTEUR, UNE METHODE
+#   D'INSTANCE APPELEE __init__
+# => ON DEFINIT LES ATTRIBUTS D'UNE INSTANCE DANS LE CONSTRUCTEUR DE SA CLASSE, EN 
+#   SUIVANT CETTE SYNTAXE: self.nom_attribut = valeur
