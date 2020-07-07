3c22045eec76515f112b302a2fe2f7a81f889aaf
danyweis <danyweismail@gmail.com>
2020-07-07 22:15:58 +0100

    props

--- vue-cli-app/src/App.vue
+++ vue-cli-app/src/App.vue
@@ -1,19 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Bonjour to {{ restaurantName }}! We are known for our freshly baked
+      bread and french pastries! Give you morning a warm start or treat
+      yourself in the middle of the day. Our butter is imported from local
+      farmers in France. Once you take your first bite, you will see why
+      everyone can't get enough!
+    </p>
+
+    <MenuItem :simpleMenu="simpleMenu" :addToShoppingCart="addToShoppingCart" />
+
+    <aside class="shopping-cart">
+      <h2>Shopping Cart: {{ items }} items</h2>
+    </aside>
+
+    <h2>Contact Us</h2>
+    <p>Address: {{ address }}</p>
+    <p>Phone: {{ phone }}</p>
+    <p>Email: {{ email }}</p>
+    <p>Hours:</p>
+    <ul>
+      <li>M-F: 06:00 to 16:00</li>
+      <li>Saturday: 07:00 to 14:00</li>
+      <li>Sunday: 07:00 to 12:00</li>
+    </ul>
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuItem from "./components/MenuItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MenuItem
+  },
+  data() {
+    return {
+      restaurantName: "Cafe avec vue",
+      address: "9274 Beurre Ave, Paris, France",
+      phone: "+08 88 88 88 88 88",
+      email: "hello@cafewithavue.bakery",
+      items: 0,
+      quantityInCart: "",
+      simpleMenu: [
+        {
+          name: "Crossiant",
+          image: {
+            source: "/images/crossiant.jpg",
+            alt: "A crossiant"
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: "French Baguette",
+          image: {
+            source: "/images/french-baguette.jpeg",
+            alt: "Four French Baguettes"
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: "Éclair",
+          image: {
+            source: "/images/eclair.jpg",
+            alt: "Chocolate Éclair"
+          },
+          inStock: false,
+          quantity: 1
+        }
+      ]
+    };
+  },
+  methods: {
+    addToShoppingCart(item) {
+      this.items += Number(item.quantity);
+    }
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
---
+++ vue-cli-app/src/components/MenuItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="menu">
+    <h2>Menu</h2>
+    <div v-for="item in simpleMenu" :key="item.name" class="menu-item">
+      <img class="menu-item__image" :src="item.image.source" :alt="item.image.alt" />
+      <div>
+        <h3>{{item.name}}</h3>
+        <p v-if="item.inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <div>
+          <label for="add-item-quantity">Quantity:</label>
+          <input v-model="item.quantity" id="add-item-quantity" type="number" />
+
+          <button @click="addToShoppingCart(item)">Add to Shopping Cart</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MenuItem",
+  props: {
+    simpleMenu: Array,
+    addToShoppingCart: Function
+  }
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
